ad56f247bde0fca070c1fc6601b70fa16ba8662c
Ezra Gildesgame <ezragildesgame@gmail.com>
2016-07-10 22:35:40 -0400

    go fmt and request all (past and future) events

--- main.go
+++ main.go
@@ -2,61 +2,60 @@
 package bands
 
 import (
-  "github.com/yujinlim/go-bandsintown/model"
-  "github.com/tj/go-debug"
-  "fmt"
+	"fmt"
+	"github.com/tj/go-debug"
+	"github.com/yujinlim/go-bandsintown/model"
 )
 
 var trace = debug.Debug("bands:log")
 
 const (
-  API_ROUTE   = "http://api.bandsintown.com/"
-  ARTIST_PATH = "artists"
-  EVENTS_PATH = "events"
-  VERSION     = "2.0"
-  URL         = API_ROUTE + ARTIST_PATH
+	API_ROUTE   = "http://api.bandsintown.com/"
+	ARTIST_PATH = "artists"
+	EVENTS_PATH = "events"
+	VERSION     = "2.0"
+	URL         = API_ROUTE + ARTIST_PATH
 )
 
 type ArtistApi interface {
-  GetArtist()       model.Artist
-  GetArtistEvents() []model.Event
+	GetArtist() model.Artist
+	GetArtistEvents() []model.Event
 }
 
 type Client struct {
-  API_KEY string
+	API_KEY string
 }
 
 // create new bandsintown api client
 func New(key string) *Client {
-  m := Client{key}
-  return &m
+	m := Client{key}
+	return &m
 }
 
 // get artist information based on artist name
-func (c *Client) GetArtist(name string) (model.Artist, error)  {
-  var artist model.Artist
-  url    := fmt.Sprintf("%s/%s?app_id=%s&api_version=%s&format=json", URL, name, c.API_KEY, VERSION)
+func (c *Client) GetArtist(name string) (model.Artist, error) {
+	var artist model.Artist
+	url := fmt.Sprintf("%s/%s?app_id=%s&api_version=%s&format=json", URL, name, c.API_KEY, VERSION)
+	if err := get(url, &artist); err != nil {
+		return artist, err
+	}
 
-  if err := get(url, &artist); err != nil {
-    return artist, err
-  }
+	trace("artist %s", artist)
 
-  trace("artist %s", artist)
-
-  return artist, nil
+	return artist, nil
 }
 
 // get artist's events by name
 func (c Client) GetArtistEvents(name string) ([]model.Event, error) {
-  var events []model.Event
-  url    := fmt.Sprintf("%s/%s/events?app_id=%s&api_version=%s&format=json", URL, name, c.API_KEY, VERSION)
+	var events []model.Event
+	url := fmt.Sprintf("%s/%s/events?app_id=%s&api_version=%s&format=json&date=all", URL, name, c.API_KEY, VERSION)
 
-  if err := get(url, &events); err != nil {
-    trace("error %s", err)
-    return events, err
-  }
+	if err := get(url, &events); err != nil {
+		trace("error %s", err)
+		return events, err
+	}
 
-  trace("events %d", len(events))
+	trace("events %d", len(events))
 
-  return events, nil
+	return events, nil
 }
