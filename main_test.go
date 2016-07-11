13bf5b2085d66303ee83eacca4c544c083ff50c6
Ezra Gildesgame <ezragildesgame@gmail.com>
2016-07-10 22:39:36 -0400

    Run go fmt

--- main_test.go
+++ main_test.go
@@ -1,55 +1,55 @@
 package bands
 
 import (
-  "testing"
-  "errors"
-  "github.com/stretchr/testify/assert"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 const (
-  APP_ID         = "barcelona"
-  VALID_ARTIST   = "Skrillex"
-  INVALID_ARTIST = "donotexists"
+	APP_ID         = "barcelona"
+	VALID_ARTIST   = "Skrillex"
+	INVALID_ARTIST = "donotexists"
 )
 
 var artists = []string{
-  "mbid_65f4f0c5-ef9e-490c-aee3-909e7ae6b2ab",
-  "Skrillex",
-  "fbid_6885814958",
+	"mbid_65f4f0c5-ef9e-490c-aee3-909e7ae6b2ab",
+	"Skrillex",
+	"fbid_6885814958",
 }
 
 var client = New(APP_ID)
 
 func TestAcceptKey(t *testing.T) {
-  assert.Equal(t, client.API_KEY, APP_ID, "app id is not the same")
+	assert.Equal(t, client.API_KEY, APP_ID, "app id is not the same")
 }
 
 func TestGetArtist(t *testing.T) {
-  for _, artistName := range artists {
-    artist, err := client.GetArtist(artistName)
+	for _, artistName := range artists {
+		artist, err := client.GetArtist(artistName)
 
-    assert.Nil(t, err)
-    assert.NotEmpty(t, artist, "artist return is incorrect")
-  }
+		assert.Nil(t, err)
+		assert.NotEmpty(t, artist, "artist return is incorrect")
+	}
 }
 
 func TestGetArtistEvents(t *testing.T) {
-  events, err := client.GetArtistEvents(VALID_ARTIST)
+	events, err := client.GetArtistEvents(VALID_ARTIST)
 
-  assert.Nil(t, err)
-  assert.True(t, len(events) > 0, "events should return more than 1")
+	assert.Nil(t, err)
+	assert.True(t, len(events) > 0, "events should return more than 1")
 }
 
 func TestGetArtistNotFound(t *testing.T) {
-  _, err := client.GetArtistEvents(INVALID_ARTIST)
+	_, err := client.GetArtistEvents(INVALID_ARTIST)
 
-  assert.Error(t, err, "does not return Unknown Artist error object")
-  assert.Equal(t, err, errors.New("Unknown Artist"))
+	assert.Error(t, err, "does not return Unknown Artist error object")
+	assert.Equal(t, err, errors.New("Unknown Artist"))
 }
 
 func TestNotFound(t *testing.T) {
-  _, err := client.GetArtist("test/test")
+	_, err := client.GetArtist("test/test")
 
-  assert.Error(t, err, "does not return 404 error")
-  assert.Equal(t, err, errors.New("status code 404"))
+	assert.Error(t, err, "does not return 404 error")
+	assert.Equal(t, err, errors.New("status code 404"))
 }
--- util.go
+++ util.go
@@ -1,61 +1,61 @@
 package bands
 
 import (
-  "net/http"
-  "io"
-  "io/ioutil"
-  "encoding/json"
-  "errors"
-  "fmt"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 )
 
 func getError(responseBody io.ReadCloser) string {
-  errorResponse := map[string][]string{}
+	errorResponse := map[string][]string{}
 
-  readBody(responseBody, &errorResponse)
+	readBody(responseBody, &errorResponse)
 
-  if len(errorResponse["errors"]) > 0 {
-    return errorResponse["errors"][0]
-  }
+	if len(errorResponse["errors"]) > 0 {
+		return errorResponse["errors"][0]
+	}
 
-  return ""
+	return ""
 }
 
 func get(url string, obj interface{}) error {
-  resp, err  := http.Get(url)
+	resp, err := http.Get(url)
 
-  if err != nil {
-    trace("error GET %s", err)
-    return err
-  }
+	if err != nil {
+		trace("error GET %s", err)
+		return err
+	}
 
-  if resp.StatusCode != 200 {
-    trace("error status code %d", resp.StatusCode)
-    
-    errMessage := getError(resp.Body)
-    if len(errMessage) > 0 {
-      return errors.New(errMessage)
-    }
-    return errors.New(fmt.Sprintf("status code %d", resp.StatusCode))
-  }
+	if resp.StatusCode != 200 {
+		trace("error status code %d", resp.StatusCode)
 
-  defer resp.Body.Close()
+		errMessage := getError(resp.Body)
+		if len(errMessage) > 0 {
+			return errors.New(errMessage)
+		}
+		return errors.New(fmt.Sprintf("status code %d", resp.StatusCode))
+	}
 
-  readBody(resp.Body, &obj)
+	defer resp.Body.Close()
 
-  return nil
+	readBody(resp.Body, &obj)
+
+	return nil
 }
 
 func readBody(responseBody io.ReadCloser, obj interface{}) error {
-  body, err := ioutil.ReadAll(responseBody);
-  if err != nil {
-    return err
-  }
+	body, err := ioutil.ReadAll(responseBody)
+	if err != nil {
+		return err
+	}
 
-  if err := json.Unmarshal(body, &obj); err != nil {
-    trace("error %s", err)
-    return err
-  }
+	if err := json.Unmarshal(body, &obj); err != nil {
+		trace("error %s", err)
+		return err
+	}
 
-  return nil
+	return nil
 }
